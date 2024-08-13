078a56eb3023271fa0b395f61f56383c0ea30ee0
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-08-13 10:32:32 -0300

    Melhorias no admin

--- Solidario.com/sistema/admin.py
+++ Solidario.com/sistema/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Doador
+from .models import Doador,CustomUser,Instituicao
 
-
+admin.site.register(CustomUser)
 admin.site.register(Doador)
+admin.site.register(Instituicao)
