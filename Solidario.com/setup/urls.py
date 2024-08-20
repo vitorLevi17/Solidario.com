88806d39c4293a616bb1e30260247bf6c7ffbeba
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-08-20 10:27:12 -0300

    Refatorando projeto
    Colocar os apps dentro da pasta de apps

--- Solidario.com/setup/urls.py
+++ Solidario.com/setup/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-import sistema.urls
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(sistema.urls))
+    path('', include(apps.sistema.urls))
 ]
