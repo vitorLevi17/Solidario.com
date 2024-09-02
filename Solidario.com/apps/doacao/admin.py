f63f09b1cd1c1bd6ba4076288088ed730c0a688f
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-09-02 17:47:00 -0300

    Admin melhorado

--- Solidario.com/apps/doacao/admin.py
+++ Solidario.com/apps/doacao/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from .models import Item,Doacao_Item,Item_Doacao_Rec,Doacao,Doacao_Rec
 
-admin.site.register(Item)
-admin.site.register(Doacao_Item)
-admin.site.register(Item_Doacao_Rec)
-admin.site.register(Doacao)
+class ListItem(admin.ModelAdmin):
+    list_display = ("nm_item","tp_item")
+
+class ListDoacaoItem(admin.ModelAdmin):
+    list_display = ("quantidade","item")
+class ListItem_Doacao_Rec(admin.ModelAdmin):
+    list_display = ("quantidade", "Item")
+class ListDoacao(admin.ModelAdmin):
+    list_display = ("urgencia",
+                    "modo_entrega",
+                    "status_doacao",
+                    "data_abertura",
+                    "recebedor",
+                    "doacao_item")
+
+admin.site.register(Item,ListItem)
+admin.site.register(Doacao_Item,ListDoacaoItem)
+admin.site.register(Item_Doacao_Rec,ListItem_Doacao_Rec)
+admin.site.register(Doacao,ListDoacao)
 admin.site.register(Doacao_Rec)
 
--- Solidario.com/apps/recebedores/admin.py
+++ Solidario.com/apps/recebedores/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from apps.recebedores.models import Recebedores
 
-admin.site.register(Recebedores)
+class ListRecebedor(admin.ModelAdmin):
+    list_display = ("usuario", "nm_recebedor","pix")
+    list_display_links = ("usuario","nm_recebedor")
+
+
+admin.site.register(Recebedores,ListRecebedor)
