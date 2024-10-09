c47b402aca22515335fb76050a8ad2c2000e608f
Levi Santos <85260420+vitorLevi17@users.noreply.github.com>
2024-10-08 21:00:36 -0300

    Update settings.py

--- Solidario.com/setup/settings.py
+++ Solidario.com/setup/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = ('l3tl2qgr%bpzlb890$3+o$+^h&rpk$zgsj37i3&!j@n7ra^*bu')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+#CSRF_TRUSTED_ORIGINS = []
 
 
 # Application definition
