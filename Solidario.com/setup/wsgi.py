9bf7f1f72450b29ccff872d6f3a31c64678b0fe7
Levi Santos <85260420+vitorLevi17@users.noreply.github.com>
2024-10-25 12:57:34 -0300

    Update wsgi.py

--- Solidario.com/setup/wsgi.py
+++ Solidario.com/setup/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for setup project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+
+path = '/home/Solidario/solidario.pythonanywhere.com/Solidario.com'
+if path not in sys.path:
+    sys.path.append(path)
+
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Solidario.com.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
-
 application = get_wsgi_application()
