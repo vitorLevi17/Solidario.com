9e381bd0df537ad70a83fe9ab437f34008894504
Levi Santos <85260420+vitorLevi17@users.noreply.github.com>
2024-11-04 11:53:26 -0300

    Update settings.py

--- Solidario.com/setup/settings.py
+++ Solidario.com/setup/settings.py
@@ -28,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-##CSRF_TRUSTED_ORIGINS = ['https://3d0f-2804-14d-7242-8070-a448-74e2-e9d7-4ad1.ngrok-free.app']
+ALLOWED_HOSTS = ['*']
+
 
 
 
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
