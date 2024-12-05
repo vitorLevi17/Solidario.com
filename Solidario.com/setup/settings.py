ae0be26682cdbd5fd11de90e08b701dad90229d1
Levi Santos <85260420+vitorLevi17@users.noreply.github.com>
2024-12-05 16:28:53 -0300

    teste

--- Solidario.com/setup/settings.py
+++ Solidario.com/setup/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = ('v6s7nqeej-=nhnb69k5n7mjf4fgu-9m2dtu9%%2y^mg*$)zx)0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
